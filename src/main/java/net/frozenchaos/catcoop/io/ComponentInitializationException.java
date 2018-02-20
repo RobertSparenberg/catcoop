630f18c0dd17b6c5d5985ab432fbc7994e376eeb
imahilus <robertsparenberg@frozenchaos.net>
2018-02-20 22:01:15 +0000

    New exception for when components fail to initialize.

---
+++ src/main/java/net/frozenchaos/catcoop/io/ComponentInitializationException.java
@@ -0,0 +1,7 @@
+package net.frozenchaos.catcoop.io;
+
+public class ComponentInitializationException extends Exception {
+    public ComponentInitializationException(Throwable cause) {
+        super(cause);
+    }
+}
