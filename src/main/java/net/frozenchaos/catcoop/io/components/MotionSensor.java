52463cb83917a1b64bdcd5995e955242936e953f
imahilus <robertsparenberg@frozenchaos.net>
2018-02-24 09:10:07 +0000

    Converted InputListener back to lambda.

--- src/main/java/net/frozenchaos/catcoop/io/components/MotionSensor.java
+++ src/main/java/net/frozenchaos/catcoop/io/components/MotionSensor.java
@@ -1,7 +1,6 @@
 package net.frozenchaos.catcoop.io.components;
 
 import com.pi4j.io.gpio.GpioPinDigitalInput;
-import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 import net.frozenchaos.catcoop.io.IoComponentListener;
 import net.frozenchaos.catcoop.io.IoManager;
@@ -23,23 +22,18 @@ public class MotionSensor extends Sensor {
     public void init() {
         logger.debug("Initializing MotionSensor on pin: "+this.pin.getPin().getAddress());
         input = this.getIoManager().getGpioController().provisionDigitalInputPin(this.pin.getPin());
-        input.addListener(new MotionSensorInputPinListener());
+        input.addListener((GpioPinListenerDigital) event -> {
+            logger.trace("MotionSensor on pin: "+MotionSensor.this.pin.getPin().getAddress()+" changed to "+event.getState());
+            for(IoComponentListener listener : MotionSensor.this.getListeners()) {
+                listener.onIoComponentEvent(MotionSensor.this, event.getState().isHigh() ? 1 : 0);
+            }
+        });
     }
 
     @Override
     public void destroy() {
         logger.debug("Destroying MotionSensor on pin: "+this.pin.getPin().getAddress());
         this.getIoManager().getGpioController().unprovisionPin(input);
-    }
-
-    private class MotionSensorInputPinListener implements GpioPinListenerDigital {
-        @Override
-        public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-            logger.trace("MotionSensor on pin: " + MotionSensor.this.pin.getPin().getAddress() + " changed to " + event.getState());
-            for(IoComponentListener listener : MotionSensor.this.getListeners()) {
-                listener.onIoComponentEvent(MotionSensor.this, event.getState().isHigh() ? 1 : 0);
-            }
-        }
     }
 
     @Override
